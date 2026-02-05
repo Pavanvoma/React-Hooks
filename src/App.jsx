1b6dc0cb5e203faec14c11d3594ebdd869147d88
voma Pavan <vomapavan@vomas-MacBook-Air.local>
2026-02-05 11:10:07 +0530

    final fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
-import { useState } from 'react'
-
 import './App.css'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-
 import FormValidation from './assets/Formvalidation'
 import { ThemeProvider } from './assets/ThemeContext'
 import ThemeToggle from './assets/ThemeToggle'
 import OptimizedList from './assets/OptmizedList'
 
-
 function App() {
-  
-
   return (
-    <>
-   
-    
-    <ThemeProvider/>
-        <ThemeToggle/>
-        <FormValidation/>
-        <OptimizedList/>
-    <ThemeProvider/>
-
-        
-    </>
+    <ThemeProvider>
+      <ThemeToggle />
+      <FormValidation />
+      <OptimizedList />
+    </ThemeProvider>
   )
 }
 
